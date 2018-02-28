8e542761e011840e91e6ac3c12d53b32363f3fa0
Denys Fontenele <denys.design@gmail.com>
2018-02-28 15:55:19 -0300

    alteração de exemplo

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -7,7 +7,20 @@ angular.module("Main").controller("mainController", function ($scope, config, ge
         password: $scope.password,
         token: $scope.token
     }
+
+    //retorna o post
     genericAPIService.post(url, json, function (ret) {
+        alert(ret);
+    })
+   
+});
+
+// 2
+angular.module("Main").controller("mainController", function ($scope, config, genericAPIService) {
+    var url = 'http://(**URL**)/login/token.php';
+    
+    //pega o get
+    genericAPIService.get(url, json, function (ret) {
         alert(ret);
     })
    
